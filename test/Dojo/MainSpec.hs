6c872344b68b26db75d1b9f830a019e0b534d898
Pedro Leal <pedrombl@gmail.com>
2013-11-05 19:48:25 -0200

    refactorando lista com replicate

--- test/Dojo/MainSpec.hs
+++ test/Dojo/MainSpec.hs
@@ -10,9 +10,9 @@ spec :: Spec
 spec = do
   describe "Calculate Bowling score" $ do
     it "should be zero when all frames are 0" $ do
-      (calculateScore [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)]) `shouldBe` 0
+      (calculateScore (replicate 10 (0, 0))) `shouldBe` 0
 
     it "should be 1 when first try of the first frame is one and the rest is 0" $ do
-      (calculateScore [(1, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)]) `shouldBe` 1
+      (calculateScore $ (1, 0) : (replicate 9 (0, 0))) `shouldBe` 1
 
 
