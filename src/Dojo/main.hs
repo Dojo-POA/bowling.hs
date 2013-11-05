38631aa1f1d6792020084228d7b4d78e834a97be
Pedro Leal <pedrombl@gmail.com>
2013-11-05 19:33:59 -0200

    funcionando para jogador ruim

---
+++ src/Dojo/main.hs
@@ -0,0 +1,6 @@
+module Dojo.Main where
+
+
+calculateScore :: [(Int, Int)] -> Int
+calculateScore frames = 
+	0
---
+++ test/Dojo/MainSpec.hs
@@ -0,0 +1,13 @@
+module Dojo.MainSpec (spec) where
+
+import Test.Hspec hiding (example)
+import Test.QuickCheck
+import Control.Exception (evaluate)
+import Dojo.Main
+
+
+spec :: Spec
+spec = do
+  describe "Calculate Bowling score" $ do
+    it "should be zero when all rows is 0" $ do
+      (calculateScore [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)]) `shouldBe` 0
