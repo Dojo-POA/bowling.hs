965e55a732566f01471312c8deba0b8ba406934b
Pedro Leal <pedrombl@gmail.com>
2013-11-05 20:58:31 -0200

    Test for multiple spares

--- test/Dojo/MainSpec.hs
+++ test/Dojo/MainSpec.hs
@@ -21,6 +21,12 @@ spec = do
     it "should be 50 when all frames are (3,2)" $ do
       (calculateScore $ replicate 10 (3, 2)) `shouldBe` 50  
 
-    it "should be 16 when first frame is (6,4) the second is (3,0) and rest is (0,0)" $ do
+    it "should be 16 when first frame is spare, the second is (3,0) and rest is (0,0)" $ do
       (calculateScore $ (6,4) : (3,0) : (replicate 8 (0, 0))) `shouldBe` 16
+
+    it "should be 32 when first and second frames spares, the third is (3,0) and rest is (0,0)" $ do
+      (calculateScore $ (6,4) : (6,4) : (3,0) : (replicate 7 (0, 0))) `shouldBe` 32
+
+
+
 
